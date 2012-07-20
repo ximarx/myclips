941ecbb3faf2cb24c4ef28f6868ae87ef465cafd
Francesco Capozzo <ximarx@gmail.com>
2012-07-20 10:08:36 +0200

    Templates -> TemplatesManager Functions -> FunctionsManager Portate le api delle vecchie definitioni di funzioni e template (con relativi attributi e condizioni)
    Pronto per utilizzo delle api *Manager nel parser

--- src/myclips/FunctionsManager.py
+++ src/myclips/FunctionsManager.py
@@ -25,8 +25,121 @@ class FunctionsManager(RestrictedManager, Observable):
         
         
 class FunctionDefinition(RestrictedDefinition):
-    def __init__(self, moduleName, defName, linkedType):
+    def __init__(self, moduleName, defName, linkedType, returnType, handler, constraints=None):
         RestrictedDefinition.__init__(self, moduleName, defName, "deffunction", linkedType)
+        self._handler = handler
+        self._returnType = returnType if isinstance(returnType, tuple) else (returnType,)
+        self._constraints = constraints if isinstance(constraints, list) else [] 
         
+    @property
+    def handler(self):
+        '''
+        Get the callable handler that realize
+        the real implementation of the function
+        '''
+        return self._handler
+    
+    @property
+    def returnType(self):
+        '''
+        Return a tuple of all return types the function
+        can use as output
+        @rtype: tuple
+        '''
+        return self._returnType
+    
+    def isValidCall(self, args):
+        for c in self._constraints:
+            # stop validation on first invalid
+            if not c.isValid(args):
+                return (False, c.getReason())
+            
+        return self.customValidation(args)
+    
+    def customValidation(self, args):
+        '''
+        Override this method to execute custom validations
+        on inputs that can't be done with constraints
+        This validation is performed as last (after all constraints check)
+        if all constraints are ok
         
+        @param args: a list/tuple of args
+        @type args: list
+        @return: a tuple (True|False, None|str:Reason of failure)
+        @rtype: tuple
+        '''
+        return (True, None)
+    
+class FunctionConstraint(object):
+    def getReason(self):
+        return ""
+    def isValid(self, args):
+        return True
+    
+class Constraint_MinArgsLength(FunctionConstraint):
+    def __init__(self, value):
+        self.value = value
         
+    def getReason(self):
+        return "expected at least {0} argument(s)".format(self.value)
+    
+    def isValid(self, args):
+        return (len(args) >= self.value)
+
+class Constraint_MaxArgsLength(FunctionConstraint):
+    def __init__(self, value):
+        self.value = value
+    
+    def getReason(self):
+        return "expected no more than {0} argument(s)".format(self.value)
+    
+    def isValid(self, args):
+        return (len(args) <= self.value)
+
+class Constraint_ExactArgsLength(FunctionConstraint):
+    def __init__(self, value):
+        self.value = value
+
+    def getReason(self):
+        return "expected exactly {0} argument(s)".format(self.value)
+    
+    def isValid(self, args):
+        return (len(args) == self.value)
+    
+class Constraint_ArgType(FunctionConstraint):
+    def __init__(self, argType, argIndex=None):
+        self.argType = argType
+        self.argIndex = argIndex
+
+    def getReason(self):
+        return "expected argument {0} to be of type {1}".format("#"+str(self.argIndex) if self.argIndex is not None else "#ALL",
+                                                                " or ".join([t.__name__ for t in self.argType])
+                                                                    if isinstance(self.argType, tuple) 
+                                                                    else self.argType.__name__
+                                                                )
+        
+    def isValid(self, args):
+        import myclips.parser.types.Types as types
+        if self.argIndex == None:
+            invalidArgs = [True if isinstance(x, self.argType)
+                                else True if isinstance(x, types.Variable)
+                                    else True if isinstance(x, types.FunctionCall)
+                                                    and self.argType in x.funcDefinition.getReturnTypes()
+                                        else True if isinstance(x, types.FunctionCall)
+                                                        and any([issubclass(retType, self.argType) for retType in x.funcDefinition.getReturnTypes()])
+                                            else False
+                           for x in args]
+            return (not any([not x for x in invalidArgs]))
+        else:
+            x = args[self.argIndex]
+            return (True if isinstance(x, self.argType)
+                                else True if isinstance(x, types.Variable)
+                                    else True if isinstance(x, types.FunctionCall)
+                                                    and self.argType in x.funcDefinition.getReturnTypes()
+                                        else True if isinstance(x, types.FunctionCall)
+                                                        and any([issubclass(retType, self.argType) for retType in x.funcDefinition.getReturnTypes()])
+                                            else False)
+
+    
+    
+    
--- src/myclips/TemplatesManager.py
+++ src/myclips/TemplatesManager.py
@@ -25,8 +25,98 @@ class TemplatesManager(RestrictedManager, Observable):
         
         
 class TemplateDefinition(RestrictedDefinition):
-    def __init__(self, moduleName, defName, linkedType):
-        RestrictedDefinition.__init__(self, moduleName, defName, "deffunction", linkedType)
+    def __init__(self, moduleName, defName, linkedType, slots=None):
+        RestrictedDefinition.__init__(self, moduleName, defName, "deftemplate", linkedType)
+        self._slots = {} if slots is None else slots
         
+    @property
+    def slots(self):
+        return self._slots
+    
+    def getSlot(self, slotName):
+        return self._slots[slotName]
+    
+        
+class SlotDefinition(object):
+    TYPE_SINGLE = "single-slot"
+    TYPE_MULTI = "multi-slot"
+    
+    def __init__(self, slotName, slotType, slotAttributes=None):
+        self._slotName = slotName
+        self._slotType = slotType
+        self._slotAttributes = dict([(attribute.attributeType, attribute) for attribute 
+                                        in (slotAttributes 
+                                                if isinstance(slotAttributes, list) 
+                                            else [])]) if not isinstance(slotAttributes, dict) else slotAttributes 
+        
+        
+    def getSlotName(self):
+        return self._slotName
+    
+    def getSlotType(self):
+        return self._slotType
+    
+    def getSlotAttributes(self):
+        return self._slotAttributes.values()
+    
+    def addSlotAttribute(self, attribute):
+        self._slotAttributes[attribute.attributeType] = attribute
+    
+    def getSlotAttribute(self, attrName):
+        return self._slotAttributes[attrName]
+    
+    def hasSlotAttribute(self, attrName):
+        return self._slotAttributes.has_key(attrName)
+    
+    @staticmethod
+    def fromParserSlotDefinition(psl):
+        import myclips.parser.types.Types as types
+        
+        sType = SlotDefinition.TYPE_SINGLE
+        if isinstance(psl, types.MultiSlotDefinition):
+            sType = SlotDefinition.TYPE_MULTI
+
+        # add Default = Nil for every slot
+        # if a definition of default value is
+        # available, it will overwrite this default            
+        attrs = {
+            Attribute_DefaultValue.attributeType : Attribute_DefaultValue(types.NullValue())
+        }
+        
+        for sattr in psl.attributes:
+            if isinstance(sattr, types.DefaultAttribute):
+                attrs[Attribute_DefaultValue.attributeType] = Attribute_DefaultValue(sattr.defaultValue)
+            elif isinstance(sattr, types.TypeAttribute):
+                attrs[Attribute_TypeConstraint.attributeType] = Attribute_TypeConstraint(sattr.allowedTypes)
+            # else unknown slot attribute type
+            # (like carinality, range or allowed-constant that are unsupported)
+            # so i just ignore it
+        
+        return SlotDefinition(psl.slotName, sType, attrs)
+
+class Attribute(object):
+    attributeType = ""
+    pass
+
+class Attribute_DefaultValue(Attribute):
+    attributeType = "default"
+    
+    def __init__(self, defaultValue):
+        self.defaultValue = defaultValue
+        
+    def getDefaultValue(self):
+        return self.defaultValue
+
+class Attribute_TypeConstraint(Attribute):
+    attributeType = "type"
+    
+    def __init__(self, allowedTypes):
+        '''
+        @param allowedTypes: a tuple of valid class types
+        '''
+        self.allowedTypes = allowedTypes
+        
+    def getAllowedTypes(self):
+        return self.allowedTypes
         
         
